d68e6fea162a5cb985deb9d15165829c98668c55
Dharun <50405830+Dharun-A@users.noreply.github.com>
2019-05-19 13:32:41 +0530

    Update Splitting_into_digits.c

--- Splitting into digits/Splitting_into_digits.c
+++ Splitting into digits/Splitting_into_digits.c
@@ -15,13 +15,13 @@ int main()
         temp = temp/10;
         factor *=10;
     }
-
+    printf("\n");
     while(factor > 1)
     {
         factor = factor/10;
         printf("%d ",number/factor);
         number %= factor;
     }
-
+    printf("\n");
     return 0;
 }
