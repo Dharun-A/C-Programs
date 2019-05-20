526fcc9108bf749aff3a6c52d496f58b9aec975e
Dharun <50405830+Dharun-A@users.noreply.github.com>
2019-05-20 10:37:13 +0530

    Create Factorial_of_a_number.c

---
+++ Factorial of a number/Factorial_of_a_number.c
@@ -0,0 +1,17 @@
+Fac#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number, temp, fact=1;
+    printf("\n\tFACTORIAL OF A NUMBER\n");
+    printf("\nEnter the number : ");
+    scanf("%d", &number);
+    temp = number;
+    while(temp > 0)
+    {
+        fact *= temp--;
+    }
+    printf("\nFactorial of %d is %d\n",number, fact);
+    return 0;
+}
